13ef2f0fe1d57d0927d9cfbe40b2b6325e02e5d6
Ragini Nevaskar <ragininevaskar.official7@gmail.com>
2024-04-04 13:48:50 +0530

    Changed the loading icon

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function loading() {
   return (
     <div className='flex justify-center align-middle'>
-        <img src='double_ring_loader.svg' alt='loading'/>
+        <img src='loader_icon.svg' alt='loading'/>
     </div>
   )
 }
